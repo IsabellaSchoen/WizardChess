79de05d5f3b633ea19598a72f75b92c900ec8793
LeonBudimovic <le391bud@htwg-konstanz.de>
2019-10-14 14:49:03 +0200

    test

---
+++ Leon/exectest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello world (pid:%d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+        char *myargs[3];
+        myargs[0] = strdup("wc");         // program: "wc" (word count)
+        myargs[1] = strdup("exectest.c"); // argument: file to count
+        myargs[2] = NULL;                 // marks end of array
+        execvp(myargs[0], myargs);        // runs word count
+        printf("this shouldn't print out");
+    } else {
+        // parent
+        int rc_wait = wait(NULL);
+        printf("hello, I am parent of %d (rc_wait:%d)    (pid:%d)\n", rc,
+            rc_wait, (int) getpid());
+    }
+    return 0;
+}
