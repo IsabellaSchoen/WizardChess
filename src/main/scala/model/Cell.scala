c4db4cd0f34810d8d8bb5861ff97ad027ec10003
LeonBudimovic <le391bud@htwg-konstanz.de>
2019-11-07 15:10:19 +0100

    more Specs

--- src/main/scala/model/Cell.scala
+++ src/main/scala/model/Cell.scala
@@ -1,17 +1,15 @@
 package model
 
-case class Cell(value: Char, value1: Int, figure: Option[Figure]= None) {
-  def isEmpty : Boolean = figure match {
+case class Cell(x: Char, y: Int, figure: Option[Figure] = None) {
+  def isEmpty: Boolean = figure match {
     case Some(x) => false
     case None => true
   }
 
-//    val king : Option[Figure] = Some(new Figure("King")
-  def set(new_figure:Figure): Cell = copy(figure = Some(new_figure))
+  //    val king : Option[Figure] = Some(Figure("King")
+  def set(new_figure: String): Cell = copy(figure = Some(Figure(new_figure)))
 
-  val x = value
-  val y = value1
-  def getX = x
-  def getY = y
-
+  override def toString: String = {
+    figure.get.toString + " " + x.toString + y.toString
+  }
 }
