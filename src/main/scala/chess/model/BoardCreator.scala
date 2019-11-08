7064747354986d7330094d3f125ad0f143fa17f7
LeonBudimovic <le391bud@htwg-konstanz.de>
2019-11-08 12:10:17 +0100

    modified cell

--- src/main/scala/chess/model/BoardCreator.scala
+++ src/main/scala/chess/model/BoardCreator.scala
@@ -13,7 +13,37 @@ class BoardCreator {
         b1.Matrix(x)(y) = Cell(x, y)
       }
     }
-    b1
+    init(b1)
+  }
+
+  def init(b1: Board): Unit = {
+    // initialization
+
+    // rooks
+    b1.matrix(0)(0) = b1.matrix(0)(0).set("rook")
+    b1.matrix(0)(7) = b1.matrix(0)(7).set("rook")
+    b1.matrix(7)(0) = b1.matrix(7)(0).set("rook")
+    b1.matrix(7)(7) = b1.matrix(7)(7).set("rook")
+
+    // knights
+    b1.matrix(1)(0) = b1.matrix(1)(0).set("knight")
+    b1.matrix(6)(0) = b1.matrix(6)(0).set("knight")
+    b1.matrix(1)(7) = b1.matrix(1)(7).set("knight")
+    b1.matrix(6)(7) = b1.matrix(6)(7).set("knight")
+
+    // bishops
+    b1.matrix(6)(7) = b1.matrix(2)(0).set("bishop")
+    b1.matrix(5)(0) = b1.matrix(5)(0).set("bishop")
+    b1.matrix(2)(7) = b1.matrix(2)(7).set("bishop")
+    b1.matrix(5)(7) = b1.matrix(5)(7).set("bishop")
+
+    // queens
+    b1.matrix(3)(0) = b1.matrix(3)(0).set("queen")
+    b1.matrix(3)(7) = b1.matrix(3)(7).set("queen")
+
+    // kings
+    b1.matrix(4)(0) = b1.matrix(4)(0).set("king")
+    b1.matrix(4)(7) = b1.matrix(4)(7).set("king")
   }
 
 }
