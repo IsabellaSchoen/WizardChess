87a731d1ce4c30dc586d89c6055b74fdc5327fef
LeonBudimovic <le391bud@htwg-konstanz.de>
2019-11-08 12:07:07 +0100

    modified cell

--- src/main/scala/chess/model/Board.scala
+++ src/main/scala/chess/model/Board.scala
@@ -9,9 +9,10 @@ case class Board(size: Int) {
   val Matrix: Array[Array[Cell]] = Array.ofDim[Cell](size, size)
 
   override def toString: String = {
-    val lineseparator = ("+-" + ("--" * size)) * size + "+\n"
-    val line = ("| " + ("x " * size)) * size + "|\n"
-    var box = "\n" + (lineseparator + (line * size)) * size + lineseparator
+    val lineseparator = ("+-" + ("--" * (2 * size - 1))) + "+\n"
+    val line = ("| " + ("x | " * size)) + "\n"
+    val sep = "--" * 2 * size + "\n"
+    var box = "\n" + (lineseparator + ((line + sep) * (size - 1))) + line + lineseparator
     for {
       row <- 0 until size
       col <- 0 until size
