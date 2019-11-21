41b68c7960ec90e359bcc5205c70555a5c5fb5d0
LeonBudimovic <le391bud@htwg-konstanz.de>
2019-11-21 13:49:46 +0100

    modified RulesSpec

--- src/test/scala/model/RulesSpec.scala
+++ src/test/scala/model/RulesSpec.scala
@@ -1,15 +1,22 @@
 package model
 
-import chess.model.{BoardCreator, Rules}
+import chess.model.{Board, BoardCreator, Rules}
 import org.scalatest.{Matchers, WordSpec}
 
 class RulesSpec extends WordSpec with Matchers{
   "a pawn" should {
     "be allowed to move" in {
-      Rules.valid(BoardCreator(8).create, 0, 1, 0, 2) should be (true)
-      Rules.valid(BoardCreator(8).create, 0, 1, 0, 3) should be (true)
-      Rules.valid(BoardCreator(8).create, 0, 6, 0, 4) should be (true)
-      Rules.valid(BoardCreator(8).create, 0, 6, 0, 5) should be (true)
+      val test: Board = BoardCreator(8).create
+      Rules.valid(test, 0, 1, 0, 2) should be (true)
+      test.move('A', '2', 'A', '3')
+      Rules.valid(test, 1, 1, 1, 3) should be (true)
+      test.move('B', '2', 'B', '4')
+      Rules.valid(test, 0, 6, 0, 4) should be (true)
+      test.move('A', '7', 'A', '5')
+      Rules.valid(test, 1, 6, 1, 5) should be (true)
+      test.move('B', '7', 'B', '6')
+      Rules.valid(test, 0, 2, 0, 3) should be (true)
+      Rules.valid(test, 0, 2, 0, 4) should be (false)
     }
   }
 }
