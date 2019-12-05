5e45c2c3f502b27c51fc224e8315a4ba1c9cb71c
LeonBudimovic <le391bud@htwg-konstanz.de>
2019-12-05 10:17:48 +0100

    modified State pattern to Pawn (not working yet)

--- src/main/scala/chess/model/Pawn.scala
+++ src/main/scala/chess/model/Pawn.scala
@@ -4,25 +4,27 @@ class Pawn(color: Char) extends Figure(color: Char) {
   override def figRule(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = PawnState.handle(x1, y1, x2, y2)
 
   object PawnState {
-    var state: () => Boolean = () => twoStep
+    var state: () => Boolean = twoStep
     var xs, ys, xe, ye = -1
     def handle(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = {
       xs = x1; ys = y1; xe = x2; ye = y2
       val backup = state
-      state = () => oneStep
+      state = oneStep
       backup()
     }
-    def twoStep: Boolean = {
+    def twoStep: () => Boolean = {
+      println("twostep")
       color match {
-        case 'B' => (oneStep || (xs == xe && ye == ys + 2))
-        case 'W' => (oneStep || (xs == xe && ye == ys - 2))
+        case 'B' => () => (oneStep() || (xs == xe && ye == ys + 2))
+        case 'W' => () => (oneStep() || (xs == xe && ye == ys - 2))
       }
     }
 
-    def oneStep: Boolean = {
+    def oneStep: () => Boolean = {
+      println("onestep")
       color match {
-        case 'B' => (xs == xe && ye == ys + 1)
-        case 'W' => (xs == xe && ye == ys - 1)
+        case 'B' => () => (xs == xe && ye == ys + 1)
+        case 'W' => () => (xs == xe && ye == ys - 1)
       }
     }
   }
