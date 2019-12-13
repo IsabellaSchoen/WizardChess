d235dd54dfa7cf4d002cea7a1632bff687de23f3
is391sch <Macedonia_2019>
2019-12-13 12:47:59 +0100

    first steps of FX

--- src/main/scala/chess/aview/gui/ScalaGui.scala
+++ src/main/scala/chess/aview/gui/ScalaGui.scala
@@ -9,28 +9,31 @@ import scalafx.scene.layout.{BorderPane, VBox}
 
 object GUI extends JFXApp {
   stage = new PrimaryStage {
-    title = "MenuBar Test"
-    scene = new Scene(300, 300) {
+    title = "WizardChess"
+    scene = new Scene(400, 400) {
 
 
-      val menuBar: MenuBar = new MenuBar { //edit
+      /*val menuBar: MenuBar = new MenuBar { //edit
         useSystemMenuBar = true
         minWidth = 100
         val menuList: Menu = new Menu("Edit") {
-          items.add(new MenuItem("Undo"))
+          items.add(new MenuItem("Exit"))
           items.add(new MenuItem("Redo"))
           items.add(new MenuItem("Save"))
         }
         menus.add(menuList)
-      }
+      }*/
 
       val rootPane: BorderPane = new BorderPane { //layout - innen drinnen aufgesplittet in top center left right and bottom
 
-        top = menuBar
-        left = new Button("djkfhsdajklhjsdfh" )
-        right = new Button("djkfhsdajklhjsdfh" )
-        center = new TextArea("ssssss")
-        bottom = new TextArea("hhhhh")
+        //top = menuBar
+        //left = new Button("djkfhsdajklhjsdfh" )
+        //right = new Button("djkfhsdajklhjsdfh" )
+        center = new TextArea("Welcome to WizardChess - this is your game!")
+        bottom = new Button("Let's go"){
+          onAction = {_ => println("changing to the next window to choose the game option")}
+        }
+        right = new Button("Rules")
 
       }
 
