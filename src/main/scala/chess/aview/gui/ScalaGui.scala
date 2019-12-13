1e44da246ea4e12e88d156a59103abcd6ca90158
is391sch <Macedonia_2019>
2019-12-13 13:42:19 +0100

    first steps of FX

--- src/main/scala/chess/aview/gui/ScalaGui.scala
+++ src/main/scala/chess/aview/gui/ScalaGui.scala
@@ -2,15 +2,20 @@ package chess.aview.gui
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
 import scalafx.scene.{Node, Scene}
 import scalafx.scene.control._
-
-import scalafx.scene.layout.{BorderPane, VBox}
+import scalafx.scene.layout.{BorderPane, HBox, VBox}
+import scalafx.scene.paint.Color._
+import scalafx.scene.text.Text
 
 object GUI extends JFXApp {
   stage = new PrimaryStage {
     title = "WizardChess"
-    scene = new Scene(400, 400) {
+
+    scene = new Scene {
+
+
 
 
       /*val menuBar: MenuBar = new MenuBar { //edit
@@ -29,14 +34,21 @@ object GUI extends JFXApp {
         //top = menuBar
         //left = new Button("djkfhsdajklhjsdfh" )
         //right = new Button("djkfhsdajklhjsdfh" )
-        center = new TextArea("Welcome to WizardChess - this is your game!")
-        bottom = new Button("Let's go"){
-          onAction = {_ => println("changing to the next window to choose the game option")}
+        center = new Text {
+          text = "Welcome to WizardChess - this is your game!"
+          style = "-fx-font-size: 48pt"
+          fill = Black
         }
-        right = new Button("Rules")
+        bottom = new Button {
+          text = "Let's go"
+          fill = GreenYellow
+          onAction = { _ => println("changing to the next window to choose the game option") }
+        }
 
+        top = new Button {
+          text = "Rules"
+        }
       }
-
       root = rootPane
     }
   }
