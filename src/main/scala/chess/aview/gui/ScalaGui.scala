e15903d24d14719d770bb6d58bd8dbaf222a69ea
is391sch <Macedonia_2019>
2019-12-13 11:52:46 +0100

    gui

--- src/main/scala/chess/aview/gui/ScalaGui.scala
+++ src/main/scala/chess/aview/gui/ScalaGui.scala
@@ -1,5 +1,15 @@
 package chess.aview.gui
 
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint.{Stops, LinearGradient}
+import scalafx.scene.text.Text
+
 class ScalaGui {
 
 }
