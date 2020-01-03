225f91b32adfc63dcea926895fb0ab4120780b8f
LeonBudimovic <le391bud@htwg-konstanz.de>
2020-01-03 20:21:42 +0100

    implementing a function to automatically insert pictures

--- src/main/scala/chess/aview/gui/Gui.scala
+++ src/main/scala/chess/aview/gui/Gui.scala
@@ -136,143 +136,140 @@ class Gui(controller: Controller) extends JFXApp with Observer {
       stackPane.getChildren.addAll(background, borderPane)
       root = stackPane
     }
-
-    def play(): Unit = {
-      stage.setFullScreen(true)
-
-      stage.scene = new Scene {
-        val stackPane: StackPane = new StackPane
-        val background = new ImageView("file:schachbrett.jpg")
-        background.setFitHeight(stage.getHeight)
-        background.setFitWidth(stage.getHeight)
-
-        val figures: GridPane = new GridPane {
-          alignment = Pos.Center
-        }
-
-        /*for (i <- 0 to 7) {
-          for (j <- 0 to 7) {
-            figures.add(new ImageView() {
-              prefWidth = (stage.getHeight - 100) / 8
-              prefHeight = (stage.getHeight - 100) / 8
-            }, i, j)
-          }
-        }*/
-
-        val grid = new GridPane
-
-        grid.setAlignment(Pos.Center)
-        fig.setAlignment(Pos.Center)
-
-        for (i <- 0 to 7) {
-          for (j <- 0 to 7) {
-            grid.add(new Button {
-              style = "-fx-background-color: transparent;"
-              onAction = { _ => click(i, j) }
-              prefWidth = (stage.getHeight - (stage.getHeight / 7.2)) / 8
-              prefHeight = (stage.getHeight - (stage.getHeight / 7.2)) / 8
-            }, i, j)
-
-            fig.add(new ImageView("file:" + controller.getFig(i, j) + ".png") {
-              fitWidth = (stage.getHeight - (stage.getHeight / 7.2)) / 8
-              fitHeight = (stage.getHeight - (stage.getHeight / 7.2)) / 8
-            }, i, j)
-          }
-        }
-
-        val tmpfig: BorderPane = new BorderPane {
-          center = fig
-        }
-
-        val tmp: BorderPane = new BorderPane {
-          center = grid
-        }
-
-        stackPane.getChildren.addAll(background, tmpfig, tmp)
-        root = stackPane
-      }
-
-      stage.setFullScreen(true)
-    }
-
-
-
-    def click(i: Integer, j: Integer): Unit = {
-      print(i + " " + j + "\n")
-      if (x != -1) {
-        println("move" + (x + 'A').toChar + (y + '1').toChar + "to" + (i+ 'A').toChar + (j + '1').toChar)
-        controller.move((x + 'A').toChar, (y + '1').toChar, (i+ 'A').toChar, (j + '1').toChar)
-        x = -1
-        y = -1
-      } else {
-        x = i
-        y = j
-      }
-    }
-
-
-
-
-    def rule() = {
-      stage.scene = new Scene {
-        val stackPane: StackPane = new StackPane
-        val tmpPane = new BorderPane {
-          top = new Button("Go Back") {
-            val stdStyle = "-fx-font-size: 25px;" +
-              "-fx-background-radius: 5em;" +
-              "-fx-min-width: 30px;" +
-              "-fx-min-height: 30px;" +
-              "-fx-max-width: 120px;" +
-              "-fx-max-height: 60px;" +
-              "-fx-padding:5;" +
-              "-fx-background-color: transparent;" +
-              "-fx-text-fill: black;"
-            style <== when(hover) choose stdStyle + "-fx-border-color: black;" otherwise stdStyle
-            prefHeight = 100
-            prefWidth = 300
-            background = null
-            onAction = { _ => {
-              println("Going back")
-              initialize()
-            }
-            }
-          }
-        }
-
-        val rect = new Rectangle {
-          width = 950
-          height = 800
-          fill = FireBrick
-          opacity = 0.5
-        }
-
-        rect.setArcHeight(100)
-        rect.setArcWidth(100)
-
-        stackPane.getChildren.addAll(tmpPane, rect,
-          new Text(Source.fromFile("Rules.txt").mkString) {
-            style = "-fx-font-size: 15px"
-          })
-        root = stackPane
-      }
-    }
-
-
-
-    /*def createMenuBar = {
-
-      val menuBar: MenuBar = new MenuBar {
-        useSystemMenuBar = true
-        minWidth = 100
-        val menuList: Menu = new Menu("Edit") {
-          items.add(new MenuItem("Exit"))
-          items.add(new MenuItem("Redo"))
-          items.add(new MenuItem("Save"))
-        }
-        menus.add(menuList)
-      }
-    }*/
   }
+
+  def play(): Unit = {
+    stage.setFullScreen(true)
+
+    stage.scene = new Scene {
+      val stackPane: StackPane = new StackPane
+      val background = new ImageView("file:schachbrett.jpg")
+      background.setFitHeight(stage.getHeight)
+      background.setFitWidth(stage.getHeight)
+
+      val figures: GridPane = new GridPane {
+        alignment = Pos.Center
+      }
+
+      /*for (i <- 0 to 7) {
+        for (j <- 0 to 7) {
+          figures.add(new ImageView() {
+            prefWidth = (stage.getHeight - 100) / 8
+            prefHeight = (stage.getHeight - 100) / 8
+          }, i, j)
+        }
+      }*/
+
+      val grid = new GridPane
+
+      grid.setAlignment(Pos.Center)
+      fig.setAlignment(Pos.Center)
+
+      for (i <- 0 to 7) {
+        for (j <- 0 to 7) {
+          grid.add(new Button {
+            style = "-fx-background-color: transparent;"
+            onAction = { _ => click(i, j) }
+            prefWidth = (stage.getHeight - (stage.getHeight / 7.2)) / 8
+            prefHeight = (stage.getHeight - (stage.getHeight / 7.2)) / 8
+          }, i, j)
+
+          fig.add(new ImageView("file:" + controller.getFig(i, j) + ".png") {
+            fitWidth = (stage.getHeight - (stage.getHeight / 7.2)) / 8
+            fitHeight = (stage.getHeight - (stage.getHeight / 7.2)) / 8
+          }, i, j)
+        }
+      }
+
+      val tmpfig: BorderPane = new BorderPane {
+        center = fig
+      }
+
+      val tmp: BorderPane = new BorderPane {
+        center = grid
+      }
+
+      stackPane.getChildren.addAll(background, tmpfig, tmp)
+      root = stackPane
+    }
+
+    stage.setFullScreen(true)
+  }
+
+
+  def click(i: Integer, j: Integer): Unit = {
+    print(i + " " + j + "\n")
+    if (x != -1) {
+      println("move" + (x + 'A').toChar + (y + '1').toChar + "to" + (i + 'A').toChar + (j + '1').toChar)
+      controller.move((x + 'A').toChar, (y + '1').toChar, (i + 'A').toChar, (j + '1').toChar)
+      x = -1
+      y = -1
+    } else {
+      x = i
+      y = j
+    }
+  }
+
+
+  def rule() = {
+    stage.scene = new Scene {
+      val stackPane: StackPane = new StackPane
+      val tmpPane = new BorderPane {
+        top = new Button("Go Back") {
+          val stdStyle = "-fx-font-size: 25px;" +
+            "-fx-background-radius: 5em;" +
+            "-fx-min-width: 30px;" +
+            "-fx-min-height: 30px;" +
+            "-fx-max-width: 120px;" +
+            "-fx-max-height: 60px;" +
+            "-fx-padding:5;" +
+            "-fx-background-color: transparent;" +
+            "-fx-text-fill: black;"
+          style <== when(hover) choose stdStyle + "-fx-border-color: black;" otherwise stdStyle
+          prefHeight = 100
+          prefWidth = 300
+          background = null
+          onAction = { _ => {
+            println("Going back")
+            initialize()
+          }
+          }
+        }
+      }
+
+      val rect = new Rectangle {
+        width = 950
+        height = 800
+        fill = FireBrick
+        opacity = 0.5
+      }
+
+      rect.setArcHeight(100)
+      rect.setArcWidth(100)
+
+      stackPane.getChildren.addAll(tmpPane, rect,
+        new Text(Source.fromFile("Rules.txt").mkString) {
+          style = "-fx-font-size: 15px"
+        })
+      root = stackPane
+    }
+  }
+
+
+  /*def createMenuBar = {
+
+    val menuBar: MenuBar = new MenuBar {
+      useSystemMenuBar = true
+      minWidth = 100
+      val menuList: Menu = new Menu("Edit") {
+        items.add(new MenuItem("Exit"))
+        items.add(new MenuItem("Redo"))
+        items.add(new MenuItem("Save"))
+      }
+      menus.add(menuList)
+    }
+  }*/
+
 
   override def update(): Unit = {
     /*fig = new GridPane
@@ -287,7 +284,7 @@ class Gui(controller: Controller) extends JFXApp with Observer {
       }
     }*/
 
-    initialize()
+    play()
   }
 }
 
