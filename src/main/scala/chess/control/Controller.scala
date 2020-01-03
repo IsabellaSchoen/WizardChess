5b8c3326fb0189e441b2bf1a670bdbcf56dbd182
IsabellaSchoen <is391sch@htwg-konstanz.de>
2020-01-03 19:35:08 +0100

    Finally the figures are done :D

--- src/main/scala/chess/control/Controller.scala
+++ src/main/scala/chess/control/Controller.scala
@@ -60,6 +60,7 @@ class Controller(var board: Board) extends Observable {
       case "rook" => "rook"
       case "horse" => "horse"
       case "bishop" => "bishop"
+      case "pawn" => "pawn_white"
       case _ => ""
     }
     tmp += "_"
