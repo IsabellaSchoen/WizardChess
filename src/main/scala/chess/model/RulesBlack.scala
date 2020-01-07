c298322ba23d1b6b2773d4ab6350b11e90048a8c
IsabellaSchoen <is391sch@htwg-konstanz.de>
2020-01-07 15:37:47 +0100

    Finished almost all the rules

--- src/main/scala/chess/model/RulesBlack.scala
+++ src/main/scala/chess/model/RulesBlack.scala
@@ -1,5 +1,10 @@
 package chess.model
 
+/**
+  * Regeln speziel für die schwarzen Figuren
+  * das Anfangsfeld des Spielfeldes darf nicht leer sein und die Farbe muss schwarz sein
+  */
+
 object RulesBlack {
   def valid(board: Board, x1: Int, y1: Int, x2: Int, y2: Int): Boolean = {
     !(board.Matrix(x1)(y1).isEmpty || board.Matrix(x1)(y1).figure.col != 'B')
--- src/main/scala/chess/model/RulesWhite.scala
+++ src/main/scala/chess/model/RulesWhite.scala
@@ -1,5 +1,10 @@
 package chess.model
 
+/**
+  * Regeln speziel für die schwarzen Figuren
+  * das Anfangsfeld des Spielfeldes darf nicht leer sein und die Farbe muss weiss sein
+  */
+
 object RulesWhite {
   def valid(board: Board, x1: Int, y1: Int, x2: Int, y2: Int): Boolean = {
     !(board.Matrix(x1)(y1).isEmpty || board.Matrix(x1)(y1).figure.col != 'W')
