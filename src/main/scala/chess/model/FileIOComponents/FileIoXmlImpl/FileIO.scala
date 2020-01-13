bbefdbe7c1d488133648f1c97502d00de37c3a22
IsabellaSchoen <is391sch@htwg-konstanz.de>
2020-01-13 23:24:48 +0100

    Starting with XML

--- src/main/scala/chess/model/FileIOComponents/FileIoXmlImpl/FileIO.scala
+++ src/main/scala/chess/model/FileIOComponents/FileIoXmlImpl/FileIO.scala
@@ -1,5 +1,28 @@
 package chess.model.FileIOComponents.FileIoXmlImpl
 
-class FileIO {
+import chess.model.BoardCreator
+import chess.model.FileIOComponents.FileIOInterface
+import chess.model.boardComponent.BoardTrait
+
+class FileIO extends FileIOInterface {
+
+
+  override def load: BoardTrait = {
+    var board: BoardTrait = null
+    val file = scala.xml.XML.loadFile("boardtrait.xml")
+  }
+
+  override def save(board: BoardTrait): Unit = ???
+
+
+
+
+  def boardToXml(board: BoardCreator) = {
+    <board size = {board.size.toString}>
+      {}
+    </board>
+  }
+
 
 }
+
