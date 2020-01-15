729d404f6abdbf9cc6ad068d0bc03fda4be2498f
IsabellaSchoen <is391sch@htwg-konstanz.de>
2020-01-15 16:55:57 +0100

    Load is missing

--- src/main/scala/chess/model/FileIOComponents/FileIoXmlImpl/FileIO.scala
+++ src/main/scala/chess/model/FileIOComponents/FileIoXmlImpl/FileIO.scala
@@ -1,53 +1,21 @@
 package chess.model.FileIOComponents.FileIoXmlImpl
 
 import java.io.{File, PrintWriter}
-
-import chess.WizardChessModule
 import chess.model.BoardCreator
 import chess.model.FileIOComponents.FileIOInterface
 import chess.model.boardComponent.BoardTrait
-import com.google.inject.Guice
-import com.google.inject.name.Names
-import net.codingwell.scalaguice.InjectorExtensions._
-
-import scala.util.Try
 import scala.xml.{Elem, PrettyPrinter}
 
 
 class FileIO extends FileIOInterface {
 
-
   //loading the board - this is freaking me out!
-  override def load: Try[Option[BoardCreator]] = {
-    var boardOption: Option[BoardCreator] = None
-
-    Try {
-      val file: Elem = scala.xml.XML.loadFile("board.xml")
-      val boardMatch = (file \\ "boardcreator" \ "@boardMatch").text.toInt
-
-      val injector = Guice.createInjector(new WizardChessModule)
-      boardMatch match {
-        case 8 => boardOption = Some(injector.instance[BoardCreator](Names.named("normal")))
-        case _ =>
-      }
+  override def load: BoardTrait = {
+    val file: Elem = scala.xml.XML.loadFile("board.xml")
 
 
-      val cellMatch = (file \\ "cell")
-      boardOption match {
-        case Some(cell) => {
-          var _cell = cell
-          for (cell <- cellMatch) {
-            val row: Int = (cell \ "@row").text.toInt
-            val col: Int = (cell \ "@col").text.toInt
-            val showFigures = (cell \ "@showFigures").text.toString
-          }
-          boardOption = Some(_cell)
-        }
-        case None =>
-      }
-      boardOption
-    }
   }
+
 
   // den Spielstand speicher - this is freaking me out! Why? Just, WHY?!
   def save(board: BoardTrait): Unit = {
